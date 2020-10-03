a31e5988c3237036d442551bdb39a01b250eb1f9
Jenna <jjr265@cornell.edu>
2020-10-03 12:57:57 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import *
+from whitenoise import WhiteNoise
+app = Flask(__name__)
+app.wsi_app = WhiteNoise(app.wsgi_app, root='static/', prefix='static/', index_file="index.html", autorefresh=True)
+
+@app.route('/', methods=['GET'])
+def home():
+  # return render_template("index.html")
+  return "Jenna Russell: https://jjr265-info4310-hw1.herokuapp.com/static/index.html"
+
+
+if __name__ == "__main__":
+  app.debug = True
+  app.run(threaded=True, port=5000)
