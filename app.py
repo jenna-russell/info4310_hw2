2af2bbd81d1447037a6918cde89e579e050c8bb8
Jenna <jjr265@cornell.edu>
2020-10-03 17:08:54 -0400

    getting dynamic map

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.wsi_app = WhiteNoise(app.wsgi_app, root='static/', prefix='static/', index_f
 @app.route('/', methods=['GET'])
 def home():
   # return render_template("index.html")
-  return "Jenna Russell: https://jjr265-info4310-hw1.herokuapp.com/static/index.html"
+  return "Jenna Russell: https://jjr265-info4310-hw2.herokuapp.com/static/index.html"
 
 
 if __name__ == "__main__":
